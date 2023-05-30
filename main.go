bbc01ec6b16bd9ce267b7e1c83636251d9e2e027
4ce <88108711+4cecoder@users.noreply.github.com>
2023-05-30 13:41:20 -0400

    red trail update
    little dots to show movement history

--- main.go
+++ main.go
@@ -2,16 +2,35 @@ package main
 
 import (
 	"image"
+	"image/color"
+	_ "image/png"
 	"math"
 	"os"
 	"time"
 
-	_ "image/png"
-
 	"github.com/faiface/pixel"
+	"github.com/faiface/pixel/imdraw"
 	"github.com/faiface/pixel/pixelgl"
 	"golang.org/x/image/colornames"
 )
+
+type Particle struct {
+	Position pixel.Vec
+	Color    color.RGBA
+}
+
+func drawParticle(win *pixelgl.Window, p Particle, size float64) {
+	imd := imdraw.New(nil)
+	imd.Color = color.RGBA{R: p.Color.R, G: p.Color.G, B: p.Color.B, A: p.Color.A}
+	imd.Push(p.Position.Sub(pixel.V(size/2, size/2)), p.Position.Add(pixel.V(size/2, size/2)))
+	imd.Rectangle(0)
+	imd.Draw(win)
+}
+func drawRedTrail(win *pixelgl.Window, particles []Particle) {
+	for _, p := range particles {
+		drawParticle(win, p, 2.0)
+	}
+}
 
 func loadPicture(path string) (pixel.Picture, error) {
 	file, err := os.Open(path)
@@ -49,6 +68,7 @@ func run() {
 	var (
 		camPos   = pixel.ZV
 		camSpeed = 500.0
+		trail    []Particle
 	)
 
 	last := time.Now()
@@ -63,25 +83,33 @@ func run() {
 		win.SetMatrix(cam)
 		mat = mat.Moved(win.Bounds().Center())
 
-		// TODO Handle logic for key presses
 		if win.Pressed(pixelgl.KeyUp) || win.Pressed(pixelgl.KeyW) {
-			camPos.Y -= camSpeed * dt
+			camPos.Y += camSpeed * dt
 			mat = mat.Rotated(win.Bounds().Center(), 0)
+			newParticle := Particle{Position: pixel.V(camPos.X, camPos.Y-1), Color: color.RGBA{R: 255, G: 0, B: 0, A: 255}}
+			trail = append(trail, newParticle)
 		}
 		if win.Pressed(pixelgl.KeyDown) || win.Pressed(pixelgl.KeyS) {
-			camPos.Y += camSpeed * dt
+			camPos.Y -= camSpeed * dt
 			mat = mat.Rotated(win.Bounds().Center(), math.Pi)
+			newParticle := Particle{Position: pixel.V(camPos.X, camPos.Y+1), Color: color.RGBA{R: 255, G: 0, B: 0, A: 255}}
+			trail = append(trail, newParticle)
 		}
-		if win.Pressed(pixelgl.KeyLeft) || win.Pressed(pixelgl.KeyD) {
-			camPos.X += camSpeed * dt
-			mat = mat.Rotated(win.Bounds().Center(), math.Pi/2)
-		}
-		if win.Pressed(pixelgl.KeyRight) || win.Pressed(pixelgl.KeyA) {
+		if win.Pressed(pixelgl.KeyLeft) || win.Pressed(pixelgl.KeyA) {
 			camPos.X -= camSpeed * dt
+			mat = mat.Rotated(win.Bounds().Center(), math.Pi/2)
+			newParticle := Particle{Position: pixel.V(camPos.X-1, camPos.Y), Color: color.RGBA{R: 255, G: 0, B: 0, A: 255}}
+			trail = append(trail, newParticle)
+		}
+		if win.Pressed(pixelgl.KeyRight) || win.Pressed(pixelgl.KeyD) {
+			camPos.X += camSpeed * dt
 			mat = mat.Rotated(win.Bounds().Center(), (math.Pi*3)/2)
+			newParticle := Particle{Position: pixel.V(camPos.X+1, camPos.Y), Color: color.RGBA{R: 255, G: 0, B: 0, A: 255}}
+			trail = append(trail, newParticle)
 		}
 
 		sprite.Draw(win, mat)
+		drawRedTrail(win, trail)
 		win.Update()
 	}
 }
