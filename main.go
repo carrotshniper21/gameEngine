193181cfa19e988fe5aebcf50b263ad1691f516c
carrot <96284645+carrotshniper21@users.noreply.github.com>
2023-05-31 00:15:47 +0000

    Update main.go

--- main.go
+++ main.go
@@ -132,7 +132,6 @@ func gameloop(win *pixelgl.Window, sprite pixel.Picture, cfg pixelgl.WindowConfi
 
 		camZoom *= math.Pow(camZoomSpeed, win.MouseScroll().Y)
 
-		drawGrid(win, grid)
 		drawRedTrail(win, trail)
 		win.Update()
 	}
